f79477f21e2d31c26b425893a78aaba871e6a0db
Rafael <rafael@macmini.local>
2018-05-03 11:22:02 -0300

    Projeto base com Firebase

---
+++ Assets/FirebaseTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FirebaseAnalytics.LogEvent ("Jogo iniciado!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Hugapup/Characters/Scripts/AC_Base.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AC_Base : MonoBehaviour {
+
+	public float interpolationMultiplier = 0.02f;
+	public float maxSpeed = 1f;
+	Animator animator;
+	Vector3 previousPosition;
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		previousPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		float currentSpeed = (currentPosition - previousPosition).magnitude;
+		speed = Mathf.Lerp (speed, currentSpeed, interpolationMultiplier);
+		float normalizedSpeed = speed / maxSpeed;
+		animator.SetFloat ("Speed", normalizedSpeed);
+		previousPosition = currentPosition;
+	}
+}
